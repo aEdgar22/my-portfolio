a15cb5765ae1ebeea74af64e2e432cf2eafe6535
edgar <a.edgar2209@gmail.com>
2022-10-11 20:54:49 -0500

    refactor/actulizacion hero banner

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -7,12 +7,9 @@ export const Data = () => {
   return (
     <HomeDataContainer>
       <h1 className="home__title">
-        Edgar De Avila <img src={hand} alt="hand" className="home__hand"/>
+        Edgar De Avila <img src={hand} alt="hand" className="home__hand" />
       </h1>
       <h3 className="home__subtitle">Frontend Developer</h3>
-      <p className="home__description">
-        I am a dedicated frontend developer passionate about the tech world!
-      </p>
 
       <a href="#contact" className="button button--flex">
         Say Hello <img className="button__icon" src={send} alt="send" />
